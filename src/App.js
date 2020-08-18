732325ad93bb9c92cfe1c997460fb27034092dee
code <mmahad280@gmail.com>
2020-08-18 12:35:37 -0400

    redirecting the posts through UsesState to map through posts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import "./App.css";
 import Post from "./Post";
 
 function App() {
-	const [posts, setPosts] = useState([]);
+	const [posts, setPosts] = useState([
+		{
+			username: "Shinichi",
+			caption: "Shiki Rhougi is fine demon",
+			imageUrl:
+				"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/96194524-6c05-4a18-96f2-da8361fde9ca/d6pal91-c0902af1-cbdf-4f14-bcb4-76a012c86948.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOTYxOTQ1MjQtNmMwNS00YTE4LTk2ZjItZGE4MzYxZmRlOWNhXC9kNnBhbDkxLWMwOTAyYWYxLWNiZGYtNGYxNC1iY2I0LTc2YTAxMmM4Njk0OC5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.XJBAjJ8UFggXlEQrLgikRikEtep-HtSoACHC_YprwAs"
+		},
+		{
+			username: "Meow",
+			caption: "Wanna drink together",
+			imageUrl:
+				"https://cdna.artstation.com/p/assets/images/images/026/835/828/large/mineo-2020-05-14-tifa.jpg?1589864340"
+		},
+		{
+			username: "Assata",
+			caption: "Sando is queen",
+			imageUrl:
+				"https://cdnb.artstation.com/p/assets/images/images/029/426/453/large/vicki-saidge42-leversedge-leila-signature-1598.jpg?1597507982"
+		}
+	]);
 
 	// Instead of hard coding posts, we want these to come from somewhere else.
 	// State will help us do this. State is a short term memory inside react.
@@ -23,6 +42,10 @@ function App() {
 	// And we declare that we will mainpulate this array (setPosts)
 	// By wrapping the array in a UseState()
 
+	// Key point: The mainpulation of the posts will happen in post.js
+	// Therefore UseState(), in this situation, is way of declaring that the posts will get mainpulated,
+	// and getting permission to run the mainpulations.
+
 	// This is an example of a hook.
 	// A hook is a small piece of code we can run.
 	// Their really powerful and functional and they allow us to use things such as state.
@@ -37,21 +60,14 @@ function App() {
 				/>
 			</div>
 			<h1>On the gram</h1>
-			<Post
-				username="Shinichi"
-				caption="Shiki Rhougi is fine demon"
-				imageUrl="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/96194524-6c05-4a18-96f2-da8361fde9ca/d6pal91-c0902af1-cbdf-4f14-bcb4-76a012c86948.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOTYxOTQ1MjQtNmMwNS00YTE4LTk2ZjItZGE4MzYxZmRlOWNhXC9kNnBhbDkxLWMwOTAyYWYxLWNiZGYtNGYxNC1iY2I0LTc2YTAxMmM4Njk0OC5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.XJBAjJ8UFggXlEQrLgikRikEtep-HtSoACHC_YprwAs"
-			/>
-			<Post
-				username="Meow"
-				caption="Wanna drink together"
-				imageUrl="https://cdna.artstation.com/p/assets/images/images/026/835/828/large/mineo-2020-05-14-tifa.jpg?1589864340"
-			/>
-			<Post
-				username="Assata"
-				caption="Sando is queen"
-				imageUrl="https://cdnb.artstation.com/p/assets/images/images/029/426/453/large/vicki-saidge42-leversedge-leila-signature-1598.jpg?1597507982"
-			/>
+
+			{posts.map((post) => (
+				<Post
+					username={post.username}
+					caption={post.caption}
+					imageUrl={post.imageUrl}
+				/>
+			))}
 		</div>
 	);
 }

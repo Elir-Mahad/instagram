b8ba2c6f2ae259ef20e6da6e7cea1c5b0764f63f
code <mmahad280@gmail.com>
2020-08-20 16:23:16 -0400

    adding the the input fileds to the image upload component

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -2,12 +2,25 @@ import React from "react";
 // import "./ImageUpload.css";
 
 function ImageUpload() {
+	const [caption, setCaption] = useState[""];
+
 	return (
 		<div>
 			<h1>ABC</h1>
 			{/* caption input */}
 			{/* file picker */}
 			{/* post button */}
+
+			<input
+				type="text"
+				placeholder="Enter a caption ..."
+				onChange={(event) => setCaption(event.target.value)}
+				value={caption}
+			/>
+
+			<input type="file" onChange={handleChange} />
+
+			<button onClick={handleUpload}>Upload</button>
 		</div>
 	);
 }

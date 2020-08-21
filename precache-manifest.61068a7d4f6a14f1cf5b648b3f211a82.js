0f6aacc1f13c3b8a6408ce2b9d50848053b8438b
code <mmahad280@gmail.com>
2020-08-21 15:52:26 -0400

    Updates

---
+++ precache-manifest.61068a7d4f6a14f1cf5b648b3f211a82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753c352a66f26ac3982f91f1e3aee924",
+    "url": "/instagram/index.html"
+  },
+  {
+    "revision": "6b4f682c512b97edf96c",
+    "url": "/instagram/static/css/main.fd9e9dba.chunk.css"
+  },
+  {
+    "revision": "ff12b3cebb036d75ac0f",
+    "url": "/instagram/static/js/2.35a2a0a5.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instagram/static/js/2.35a2a0a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b4f682c512b97edf96c",
+    "url": "/instagram/static/js/main.b2d50d32.chunk.js"
+  },
+  {
+    "revision": "e66d7436a0aabca8e3136db51c4bc084",
+    "url": "/instagram/static/js/main.b2d50d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060ad16ba7f5db70f5a5",
+    "url": "/instagram/static/js/runtime-main.a87bf05b.js"
+  }
+]);

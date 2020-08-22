dfaab7c839af46f0d08150e7fbbeb804f0d68493
code <mmahad280@gmail.com>
2020-08-22 15:49:45 -0400

    Updates

--- precache-manifest.7ec395e0d409b360f17f80ba131c49f4.js
+++ precache-manifest.7ec395e0d409b360f17f80ba131c49f4.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "192805b4adf5bf9473af121a8aa60b67",
+    "revision": "5520d118845a8b0f7455c14f7ace84c2",
     "url": "/instagram/index.html"
   },
   {
-    "revision": "eca038ed8b0857ea35cb",
+    "revision": "e42386100b31ac56dd68",
     "url": "/instagram/static/css/main.fd9e9dba.chunk.css"
   },
   {
-    "revision": "924659744c94a81527a0",
-    "url": "/instagram/static/js/2.b6f167d8.chunk.js"
+    "revision": "ff12b3cebb036d75ac0f",
+    "url": "/instagram/static/js/2.35a2a0a5.chunk.js"
   },
   {
     "revision": "5f8be6edcb631ea3c834b8459dc2c264",
-    "url": "/instagram/static/js/2.b6f167d8.chunk.js.LICENSE.txt"
+    "url": "/instagram/static/js/2.35a2a0a5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "eca038ed8b0857ea35cb",
-    "url": "/instagram/static/js/main.f745648f.chunk.js"
+    "revision": "e42386100b31ac56dd68",
+    "url": "/instagram/static/js/main.3ec8a091.chunk.js"
   },
   {
     "revision": "e66d7436a0aabca8e3136db51c4bc084",
-    "url": "/instagram/static/js/main.f745648f.chunk.js.LICENSE.txt"
+    "url": "/instagram/static/js/main.3ec8a091.chunk.js.LICENSE.txt"
   },
   {
     "revision": "060ad16ba7f5db70f5a5",
